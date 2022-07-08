281c3edc51b082c6d07f9627b8949c7dbad87043
Ebrahim Ameen <ebrahim.ameen@bosch.com>
2022-07-08 15:51:05 +0200

    fix: remove dapr  --runtime-version 1.8.0-rc.3

--- .vscode/scripts/ensure-dapr.sh
+++ .vscode/scripts/ensure-dapr.sh
@@ -35,7 +35,7 @@ if ! [[ $version =~ ^([0-9]{1,2})\.([0-9]{1,2})\.([0-9]{1,2}) ]]; then
       fi
 
       dapr uninstall
-      dapr init --runtime-version 1.8.0-rc.3
+      dapr init
 else
       echo "Dapr is already installed and initialized, skipping setup."
 fi
