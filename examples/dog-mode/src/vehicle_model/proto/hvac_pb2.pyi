c7b1ab4b2784d5bc6c27a545693f7a38ffd0f1c3
kse3hi <serhii.korchynskyi@etas.com>
2023-11-26 19:24:50 +0000

    Fix self review findings

--- examples/dog-mode/src/vehicle_model/proto/hvac_pb2.pyi
+++ examples/dog-mode/src/vehicle_model/proto/hvac_pb2.pyi
@@ -1,4 +1,4 @@
-# Copyright (c) 2022-2023 Contributors to the Eclipse Foundation
+# Copyright (c) 2022 Robert Bosch GmbH and Microsoft Corporation
 #
 # This program and the accompanying materials are made available under the
 # terms of the Apache License, Version 2.0 which is available at
@@ -26,17 +26,12 @@ import typing_extensions
 DESCRIPTOR: google.protobuf.descriptor.FileDescriptor
 
 class _AcStatus:
-    ValueType = typing.NewType("ValueType", builtins.int)
+    ValueType = typing.NewType('ValueType', builtins.int)
     V: typing_extensions.TypeAlias = ValueType
-
-class _AcStatusEnumTypeWrapper(
-    google.protobuf.internal.enum_type_wrapper._EnumTypeWrapper[_AcStatus.ValueType],
-    builtins.type,
-):
+class _AcStatusEnumTypeWrapper(google.protobuf.internal.enum_type_wrapper._EnumTypeWrapper[_AcStatus.ValueType], builtins.type):
     DESCRIPTOR: google.protobuf.descriptor.EnumDescriptor
     OFF: _AcStatus.ValueType  # 0
     ON: _AcStatus.ValueType  # 1
-
 class AcStatus(_AcStatus, metaclass=_AcStatusEnumTypeWrapper):
     pass
 
@@ -44,68 +39,53 @@ OFF: AcStatus.ValueType  # 0
 ON: AcStatus.ValueType  # 1
 global___AcStatus = AcStatus
 
+
 class SetAcStatusRequest(google.protobuf.message.Message):
     """*
     @brief
     """
-
     DESCRIPTOR: google.protobuf.descriptor.Descriptor
     STATUS_FIELD_NUMBER: builtins.int
     status: global___AcStatus.ValueType
     """The desired status of A/C"""
 
-    def __init__(
-        self,
+    def __init__(self,
         *,
         status: global___AcStatus.ValueType = ...,
-    ) -> None: ...
-    def ClearField(
-        self, field_name: typing_extensions.Literal["status", b"status"]
-    ) -> None: ...
-
+        ) -> None: ...
+    def ClearField(self, field_name: typing_extensions.Literal["status",b"status"]) -> None: ...
 global___SetAcStatusRequest = SetAcStatusRequest
 
 class SetAcStatusReply(google.protobuf.message.Message):
     """*
     @brief
     """
-
     DESCRIPTOR: google.protobuf.descriptor.Descriptor
-    def __init__(
-        self,
-    ) -> None: ...
-
+    def __init__(self,
+        ) -> None: ...
 global___SetAcStatusReply = SetAcStatusReply
 
 class SetTemperatureRequest(google.protobuf.message.Message):
     """*
     @brief
     """
-
     DESCRIPTOR: google.protobuf.descriptor.Descriptor
     TEMPERATURE_FIELD_NUMBER: builtins.int
     temperature: builtins.float
     """The desired cabin temperature in degree Celsius"""
 
-    def __init__(
-        self,
+    def __init__(self,
         *,
         temperature: builtins.float = ...,
-    ) -> None: ...
-    def ClearField(
-        self, field_name: typing_extensions.Literal["temperature", b"temperature"]
-    ) -> None: ...
-
+        ) -> None: ...
+    def ClearField(self, field_name: typing_extensions.Literal["temperature",b"temperature"]) -> None: ...
 global___SetTemperatureRequest = SetTemperatureRequest
 
 class SetTemperatureReply(google.protobuf.message.Message):
     """*
     @brief
     """
-
     DESCRIPTOR: google.protobuf.descriptor.Descriptor
-    def __init__(
-        self,
-    ) -> None: ...
-
+    def __init__(self,
+        ) -> None: ...
 global___SetTemperatureReply = SetTemperatureReply
