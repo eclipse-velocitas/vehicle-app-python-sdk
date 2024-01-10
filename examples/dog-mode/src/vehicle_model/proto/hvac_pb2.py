6ff3c6cb53479a33236703985b5f392f3e10aa03
Dennis Meister <dennis.meister@bosch.com>
2024-01-10 14:45:07 +0100

    Update license header to 2024
    Signed-off-by: Dennis Meister <dennis.meister@bosch.com>

--- examples/dog-mode/src/vehicle_model/proto/hvac_pb2.py
+++ examples/dog-mode/src/vehicle_model/proto/hvac_pb2.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2023 Contributors to the Eclipse Foundation
+# Copyright (c) 2023-2024 Contributors to the Eclipse Foundation
 # This program and the accompanying materials are made available under the
 # terms of the Apache License, Version 2.0 which is available at
 # https://www.apache.org/licenses/LICENSE-2.0.
